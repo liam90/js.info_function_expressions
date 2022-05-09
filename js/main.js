0fb77a77770b56a6160610cdeb18a489ff6abcac
Liam Mandeville <liamman1990@gmail.com>
2022-05-09 15:03:03 -0700

    finished the reading and examples

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ let myFunc = sayHi;
 
 // myFunc();
 
+// Callback Functions
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+function showOk() {
+  alert('You agree.');
+}
+function showCancel() {
+  alert('You canceled the execution.');
+}
+
+// ask("Do you agree", showOk, showCancel);
+
+// Shorter callback example
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+// ask(
+//   "Do you agree ?",
+//   function() { alert('You agree.');},
+//   function() { alert("You canceled the execution.");}
+// );
+
+// Function Declaration
+function sum(a, b) {
+  return a + b;
+}
+// Function Expression
+let sum = function(a, b) {
+  return a + b;
+};
